3bd5bded2dcc32d131e3414c7046da4d4b7ca1c5
Aos1n <aosin.tetrahead@gmail.com>
2020-12-14 13:37:09 +0000

    controle travis

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import Status from "../components/Status"
 const Index = () => (
   <Layout>
     <Status />
-    <View title="Bienvenue chez IT Corail">
+    <View title="Bienvenue chez IT">
       <p>
        Aos gère tout   IT Corail se veut un partenaire de services informatiques pour toutes les entreprises, administrations et associations en région liégeoise. 
       </p>
